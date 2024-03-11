6bc03151920a43bc0f9e95cc425acb45eba374c2
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2024-03-11 09:52:06 +0000

    Initial commit

---
+++ webpack.build.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+
+const common = require('./webpack.common');
+
+module.exports = merge(common, {
+  mode: 'production',
+  devtool: 'source-map',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+});
